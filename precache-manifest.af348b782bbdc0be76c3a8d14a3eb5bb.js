49998cf9edbadc1b387130dc7236157560308b13
Marrichandu <63691632+Marrichandu@users.noreply.github.com>
2020-09-08 17:18:37 +0530

    first app

---
+++ precache-manifest.af348b782bbdc0be76c3a8d14a3eb5bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e1cca4a00d59e127b0c3147712280c",
+    "url": "/Marrichandu/coursera-test1index.html"
+  },
+  {
+    "revision": "9d30a36a59457f0464e9",
+    "url": "/Marrichandu/coursera-test1static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/Marrichandu/coursera-test1static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Marrichandu/coursera-test1static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d30a36a59457f0464e9",
+    "url": "/Marrichandu/coursera-test1static/js/main.8d8b47c1.chunk.js"
+  },
+  {
+    "revision": "0180a76e10cc09c1d46e",
+    "url": "/Marrichandu/coursera-test1static/js/runtime-main.4d3c8c61.js"
+  }
+]);
